7ee07d4d739218504068760450401702f29689aa
Hassaan Ali <hassaanali632@gmail.com>
2024-03-22 20:16:20 +0500

    Login View Modification

--- xpoarena/views.py
+++ xpoarena/views.py
@@ -250,9 +250,14 @@ def login_view(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return Response({"detail": "Login successful"}, status=status.HTTP_200_OK)
+        try:
+            UserProfile.objects.get(user=user)
+            has_profile = True
+        except UserProfile.DoesNotExist:
+            has_profile = False
+        return Response({"detail": "Login successful", "has_profile": has_profile}, status=200)
     else:
-        return Response({"detail": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Invalid credentials"}, status=400)
     
 
 @api_view(['POST'])
